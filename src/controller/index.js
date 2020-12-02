f559920a435e554ae04484637a40663d07781c18
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2020-12-02 16:25:46 +0200

    fix marge

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,24 +1,28 @@
 const fetch = require('node-fetch');
 
-// const { appid, appkey } = process.env;
-
-const appid = 'cbbe1a4d';
-const appkey = 'ba846de8038642ef3e5c1e2ff1dd39e3';
+const { appid, appkey } = process.env;
 
 const searchFood = (search) => {
-  fetch(`https://api.edamam.com/search?q=${search}&app_id=${appid}&app_key=${appkey}&from=0&to=3&calories=591-722&health=alcohol-free`)
+  fetch(
+    `https://api.edamam.com/search?q=${search}&app_id=${appid}&app_key=${appkey}`,
+  )
     .then((response) => response.json());
 };
 
 const getBySearch = (req, res) => {
   const { search } = req.body;
-  searchFood(search).then((result) => {
+  searchFood(search)
+    .then((result) => {
+      // eslint-disable-next-line no-console
+      console.log(result);
+      res.json(result);
+      // eslint-disable-next-line no-console
+    })
     // eslint-disable-next-line no-console
-    console.log(result);
-    res.json(result);
-  // eslint-disable-next-line no-console
-  }).catch(console.error);
+    .catch(console.error);
 };
+
+// searchFood(searchTest);
 
 module.exports = {
   getBySearch,
